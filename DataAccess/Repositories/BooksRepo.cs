85415dfed4d47a050eb01b40a6248b1fc84df32a
Toni Dedić <dedictoni1@gmail.com>
2020-02-19 10:22:16 +0100

    Import project

---
+++ DataAccess/Repositories/BooksRepo.cs
@@ -0,0 +1,58 @@
+﻿using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class BooksRepo
+    {
+        public List<Book> GetAll()
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                return db.Books.Include(x => x.Genre).ToList();
+            }
+        }
+
+        public Book GetById(int id)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                return db.Books.Find(id);
+            }
+        }
+
+        public void Create(Book book)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+            }
+        }
+
+        public void Update(Book book)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                Book book = db.Books.Find(id);
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Entities/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(): base("KnjizaraConnStr")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
