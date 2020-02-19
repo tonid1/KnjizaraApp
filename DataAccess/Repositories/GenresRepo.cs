0477558cbca3345f1ecb6ad1b81337d286801aa4
Toni Dedić <dedictoni1@gmail.com>
2020-02-19 10:08:15 +0100

    Importing project

---
+++ DataAccess/Repositories/GenresRepo.cs
@@ -0,0 +1,58 @@
+﻿using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class GenresRepo
+    {
+        public List<Genre> GetAll()
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                return db.Genres.ToList();
+            }
+        }
+
+        public Genre GetById(int id)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                return db.Genres.Find(id);
+            }
+        }
+
+        public void Create(Genre genre)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+            }
+        }
+
+        public void Update(Genre genre)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                Genre genre = db.Genres.Find(id);
+                db.Genres.Remove(genre);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Entities/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Author { get; set; }
+
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Range(1, 2000, ErrorMessage = "This field cannot have value 0")]
+        public int Pages { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Range(1, 2000, ErrorMessage = "This field cannot have value 0")]
+        [UIHint("DisplayCurrency")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Range(1, 2000, ErrorMessage = "This field cannot have value 0")]
+        [Display(Name = "In stock")]
+        public int InStock { get; set; }
+    }
+}
